2b006dfbf4382b64f81ac26e64b002cf036bbb51
Mariana Torres <mariana.torres@certacon.com.br>
2024-11-08 14:16:11 -0300

    Adiciona saída de valores

--- src/variaveisPrimitivas/tiposPrimitivos.java
+++ src/variaveisPrimitivas/tiposPrimitivos.java
@@ -14,13 +14,20 @@ public class tiposPrimitivos {
         short itens = 2;
         int quantEstoque = 2366;
         long numeroMuitoGrande = 55555555L;
+        System.out.println("nota = " + nota);
+        System.out.println("itens = " + itens);
+        System.out.println("quantidade em estoque = " + quantEstoque);
+        System.out.println("número muito grande = " + numeroMuitoGrande);;
 
         //Decimais
         float peso = 1.5f;
         double preco = 24.99;
+        System.out.println("peso = "+ peso);
+        System.out.println("preço = " + preco);
     
         //Booleanos ou Boolean
         boolean verdadeiro = true;
         boolean falso = false;
+        System.out.println("boolean = " + verdadeiro + " ou " + falso);
     }
 }
