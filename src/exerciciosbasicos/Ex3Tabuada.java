a4eef5f73d90b5ee870cdee0e7344088641209cd
Mariana Torres <mariana.torres@certacon.com.br>
2024-11-08 16:14:17 -0300

    Cria arquivo do exercício 3

---
+++ src/exerciciosbasicos/Ex3Tabuada.java
@@ -0,0 +1,19 @@
+package exerciciosbasicos;
+
+import java.util.Scanner;
+
+public class Ex3Tabuada {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num;
+        
+        System.out.println("Digite um número");
+        num = scan.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num*i));
+        }
+
+        scan.close();
+    }
+}
