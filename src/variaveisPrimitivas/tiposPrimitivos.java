77ffc7112a96b88c9dc38eb7a134e8ab806d046e
Mariana Torres <mariana.torres@certacon.com.br>
2024-11-08 13:51:01 -0300

    Adiciona o package e arquivo sobre variaveis primitivas

---
+++ src/variaveisPrimitivas/tiposPrimitivos.java
@@ -0,0 +1,11 @@
+package projetoCurso.variaveisPrimitivas;
+
+public class tiposPrimitivos {
+    public static void main(String[] args) {
+        char primeiraLetraNome = 'M';
+        char primeiraLetraNomeASCII = 77;
+
+        System.out.println(primeiraLetraNome);
+        System.out.println(primeiraLetraNomeASCII);
+    }
+}
