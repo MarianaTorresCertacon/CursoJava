3de2691856dee547375ccd1930b177e70e0a8833
Mariana Torres <mariana.torres@certacon.com.br>
2024-11-08 13:56:11 -0300

    Atualiza o arquivo tiposPrimitivos.java

--- src/variaveisPrimitivas/tiposPrimitivos.java
+++ src/variaveisPrimitivas/tiposPrimitivos.java
@@ -1,11 +1,26 @@
-package projetoCurso.variaveisPrimitivas;
+package variaveisPrimitivas;
 
 public class tiposPrimitivos {
     public static void main(String[] args) {
+        //Caracteres Characters
         char primeiraLetraNome = 'M';
         char primeiraLetraNomeASCII = 77;
 
         System.out.println(primeiraLetraNome);
         System.out.println(primeiraLetraNomeASCII);
+
+        //Inteiros Integer
+        byte nota = 10; 
+        short itens = 2;
+        int quantEstoque = 2366;
+        long numeroMuitoGrande = 55555555L;
+
+        //Decimais
+        float peso = 1.5f;
+        double preco = 24.99;
+    
+        //Booleanos ou Boolean
+        boolean verdadeiro = true;
+        boolean falso = false;
     }
 }
